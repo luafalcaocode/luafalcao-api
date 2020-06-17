764f902d7417b9299821364db96f2481878122e5
Luã Falcão <lcodedeveloper@outlook.com>
2020-06-17 10:21:40 -0300

    Refactoring of the code to be more organized and included config.public.js file with constants used by application

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 const express = require('express');
+const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const http = require('http');
 const https = require('https');
-
 const solicitacaoServicoRoutes = require('./routes/solicitacao-servico');
-
 const credentials = { key: fs.readFileSync('key.pem', 'utf8'), cert: fs.readFileSync('cert.pem', 'utf8') };
-const app = express();
 const httpServer = http.createServer(app);
 const httpsServer = https.createServer(credentials, app);
 
@@ -16,7 +14,5 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.use(cors());
 app.use('/api/servicos', solicitacaoServicoRoutes);
 
-
 httpServer.listen(7777);
 httpsServer.listen(9999);
-// app.listen(4100);
--- routes/solicitacao-servico.js
+++ routes/solicitacao-servico.js
@@ -1,69 +1,89 @@
-const express = require('express');
-const formidable = require('formidable');
-const path = require('path');
-const fs = require('fs');
-const nodemailer = require('nodemailer');
-
+const express = require("express");
+const formidable = require("formidable");
+const path = require("path");
+const fs = require("fs");
+const nodemailer = require("nodemailer");
 const router = express.Router();
 
-router.post('/solicitacao', (request, response, next) => {
-    const form = new formidable.IncomingForm();
-    form.multiples = true;
-    form.uploadDir = path.join(__dirname, '../', 'uploads');
-    form.on('file', (field, file) => {
-        fs.rename(file.path, path.join(form.uploadDir, file.name), () => {
-        });
-    });
-    form.parse(request, (err, fields, files) => {
-        const transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                user: 'adicionar_email_disparador_aqui',
-                pass: 'adicionar_senha_disparador_aqui'
-            }
-        });
-        const mailOptions = {
-            from: 'adicionar_email_remetente_aqui',
-            to: 'adicionar_email_destinatario_aqui',
-            subject: 'solicitação de serviço',
-            attachments: [
+const config = require('../config.public');
 
-            ],
-            html: `
-                    <h1>Solicitação de Serviço</h1>
-                        <ul>
-                        <li>${fields.solicitante}</li>
-                        <li>${fields.email}</li>
-                        <li>${fields.observacoes}</li>
-                        <li>${fields.tipoDeProjeto}</li>
-                   </ul>`
-        };
-        let stream;
-        fs.readdir(path.join(__dirname, '../', 'uploads'), (err, files) => {
-            files.forEach(file => {
-                stream = fs.createReadStream(path.join(__dirname, '../', 'uploads', file));
-                mailOptions.attachments.push({ filename: file, content: stream });
-            });
-            transporter.sendMail(mailOptions, (err, info) => {
-                files.forEach(file => {
-                    fs.unlink(path.join(__dirname, '../', 'uploads', '/', file), err => {
-                        if (err)
-                            console.log(err);
-                    });
-                });
-                if (!err) {
-                    response.status(200).send({ message: 'the data was sent successfuly!', success: true });
-                }
-                else {
-                    response.status(400).send({ message: `${err.name}: ${err.message}`, success: false });
-                }
-            });
-        });
+router.post("/solicitacao", (request, response, next) => {
+  const form = new formidable.IncomingForm();
+  form.multiples = true;
+  form.uploadDir = path.join(__dirname, "../", "uploads");
+
+  form.on("file", (field, file) => {
+    fs.rename(file.path, path.join(form.uploadDir, file.name), () => {});
+  });
+
+  form.parse(request, (err, fields, files) => {
+    const transporter = nodemailer.createTransport({
+      service: config.email.smtp,
+      auth: {
+        user: config.email.credentials.user,
+        pass: config.email.credentials.password,
+      },
     });
+
+    const mailOptions = {
+      from: config.email.from,
+      to: config.email.to,
+      subject: config.email.subject,
+      attachments: [],
+      html: `
+                    <table style="width: 100%">
+                        <thead>
+                            <tr>
+                                <td style="font-size: 26px;font-weight: bold;background: #25364E;color: #11DED8">Solicitação de serviço</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td style="border: solid 1px black;height: 30px;font-size: 18px;padding: 3px;background: #25364E;color: #11DED8""><strong>Solicitante:</strong> ${fields.solicitante} </td>
+                            </tr>
+                            <tr>
+                                <td style="border: solid 1px black;height: 30px;font-size: 18px;padding: 3px;background: #25364E;color: #11DED8""><strong>E-mail:</strong> ${fields.email} </td>
+                            </tr>
+                            <tr>
+                                <td style="border: solid 1px black;height: 30px;font-size: 18px;padding: 3px;background: #25364E;color: #11DED8""><strong>Tipo de Projeto:</strong> ${fields.tipoDeProjeto} </td>
+                            </tr>
+                            <tr>
+                                <td style="border: solid 1px black;height: 30px;font-size: 18px;padding: 3px;background: #25364E;color: #11DED8""><strong>Observações:</strong> ${fields.observacoes} </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                  `,
+    };
+
+    let stream;
+    fs.readdir(config.uploadDir, (err, files) => {
+      files.forEach((file) => {
+        stream = fs.createReadStream(path.join(config.uploadDir, file));
+        mailOptions.attachments.push({ filename: file, content: stream });
+      });
+      transporter.sendMail(mailOptions, (err, info) => {
+        files.forEach((file) => {
+          fs.unlink(path.join(config.uploadDir, "/", file), (err) => {
+            if (err) console.log(err);
+          });
+        });
+
+        if (!err) {
+          response
+            .status(config.statusCode.success)
+            .send({ message: "the data was sent successfuly!", success: true });
+        } else {
+          response
+            .status(config.statusCode.bad)
+            .send({ message: `${err.name}: ${err.message}`, success: false });
+        }
+      });
+    });
+  });
 });
 
-router.get('/solicitacao', (request, response, next) => {
-    response.send('<h1>API works</h1>');
+router.get("/solicitacao", (request, response, next) => {
+  response.send("<h1>API works</h1>");
 });
 
 module.exports = router;
